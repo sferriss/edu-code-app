d0071cd1ba405bd270d3d624239de73fd5525da9
Sandro <sandro_junior@hotmail.com>
2023-07-12 22:29:32 -0300

    feat: botoes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,28 @@
 import "../styles/navbar.css"
 import Logo from "../assets/a-rocket-5-no-background.png"
 import {Link} from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export function Navbar() {
+    const location = useLocation();
+    const [activePath, setActivePath] = useState(location.pathname);
+
+    useEffect(() => {
+        setActivePath(location.pathname);
+    }, [location]);
+
     return (
         <nav className="navbar-container">
             <div className="navbar-sub-container">
                 <Link className="logo" to="/">
                     <img src={Logo} alt="logo"/>
                 </Link>
-                <Link className={`navbar-link ${location.pathname === "/" ? "active-link" : ""}`} to="/">
+                <Link className={`navbar-link ${activePath === '/' ? 'active-link' : ''}`} to="/">
                     Exercícios
+                </Link>
+                <Link className={`navbar-link ${activePath === '/compiler' ? 'active-link' : ''}`} to="/compiler">
+                    Compiler
                 </Link>
             </div>
             <div className="navbar-sub-container">
