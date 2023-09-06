95c18a882a1d2541b0f86a2f06d0acc334b3888b
Sandro Ferri <sandro.ferri@cwi.com.br>
2023-09-06 17:37:11 -0300

    fix: corrigido card das questoes

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,12 +2,13 @@ import "../styles/cards.css"
 import {FullQuestionResponse} from "../interfaces/responses/fullQuestionResponse.ts";
 import ExampleCard from "./ExampleCard.tsx";
 import React from "react";
+import MarkdownViewer from "./MarkdownViewer.tsx";
 
 interface QuestionCardProps {
     question: FullQuestionResponse | undefined;
 }
 
-const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
+const QuestionCard: React.FC<QuestionCardProps> = ({question}) => {
     function formatDifficulty(difficulty: string | undefined): string {
         switch (difficulty) {
             case "Easy":
@@ -23,13 +24,15 @@ const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
 
     return (
         <div className="question-card">
-            <div className="title-container">
+            <div>
                 <h3>{question?.title} - {formatDifficulty(question?.difficult)}</h3>
             </div>
-            <span>{question?.description}</span>
-            {question?.examples && question?.examples.map((e, i) => (
-                <ExampleCard key={e.id} index={i} example={e}/>
-            ))}
+            <div>
+                <MarkdownViewer content={question?.description || ""}/>
+                {question?.examples && question?.examples.map((e, i) => (
+                    <ExampleCard key={e.id} index={i} example={e}/>
+                ))}
+            </div>
         </div>
     )
 }
